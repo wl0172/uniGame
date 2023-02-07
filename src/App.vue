84e3e5d3d626d313cb8e5ec63962572cfdea538f
Jiawei.Liu <Jiawei.Liu@ccegroup.cn>
2023-02-07 15:29:51 +0800

    fix: init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <script>
-export default {
-  onLaunch: function () {
-    console.log('App Launch')
-  },
-  onShow: function () {
-    console.log('App Show')
-  },
-  onHide: function () {
-    console.log('App Hide')
-  },
-}
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
 </script>
 
 <style>
-/*每个页面公共css */
+	uni-page-body{
+		width: 100%;
+		height: 100%;
+		background: #01441b54;
+		display: flex;
+		align-items: center;
+		position: relative;
+	}
+	/*每个页面公共css */
 </style>
